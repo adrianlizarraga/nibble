e2f9e1dec9d6f3d3ea03d1e11128c9cb4fa379f4
adrianlizarraga <adrianlm2@gmail.com>
2021-02-17 04:52:55 -0800

    Convert recursive c-style comment lexing to iterative form. Added error message accumulation for lexer.

--- src/lexer.c
+++ src/lexer.c
@@ -1,10 +1,21 @@
 #include "lexer.h"
 #include <assert.h>
+#include <stdarg.h>
 #include <stdbool.h>
+
 
 static bool is_whitespace(char c)
 {
     return (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r') || (c == '\v');
+}
+
+static void lexer_error(Lexer* lexer, const char* format, ...)
+{
+    va_list vargs;
+
+    va_start(vargs, format);
+    vsnprintf(lexer->errors[lexer->num_errors++], LEXER_MAX_ERROR_LEN, format, vargs);
+    va_end(vargs);
 }
 
 static void process_newline(Lexer* lexer)
@@ -18,26 +29,33 @@ static void process_newline(Lexer* lexer)
 
 static void skip_c_comment(Lexer* lexer)
 {
-    if ((lexer->at[0] == '/') && (lexer->at[1] == '*')) {
-	lexer->at += 2;
+    assert(lexer->at[0] == '/');
+    assert(lexer->at[1] == '*');
+    lexer->at += 2;
 
-	while (lexer->at[0] && !((lexer->at[0] == '*') && (lexer->at[1] == '/'))) {
+    int level = 1;
+    while (lexer->at[0] && (level > 0)) {
 
-	    // Nested c-style comment
-	    if ((lexer->at[0] == '/') && (lexer->at[1] == '*')) {
-	        skip_c_comment(lexer);
-	    }
-	    else if (lexer->at[0] == '\n') {
-		process_newline(lexer);
-	    }
-	    else {
-		lexer->at++;
-	    }
-	}
-
-	if (lexer->at[0]) {
+	// Nested c-style comment
+	if ((lexer->at[0] == '/') && (lexer->at[1] == '*')) {
+	    level += 1;
 	    lexer->at += 2;
 	}
+	// End of one level of c-style comments
+	else if ((lexer->at[0] == '*') && (lexer->at[1] == '/')) {
+	    level -= 1;
+	    lexer->at += 2;
+	}
+	else if (lexer->at[0] == '\n') {
+	    process_newline(lexer);
+	}
+	else {
+	    lexer->at++;
+	}
+    }
+
+    if (level > 0) {
+	lexer_error(lexer, "[ERROR] Missing closing '*/' for c-style comment.\n");
     }
 }
 
