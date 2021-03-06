d44556ba8cf76b162b3a35fe19043da16530b722
adrianlizarraga <adrianlm2@gmail.com>
2021-03-06 09:39:27 -0800

    Specialize hash_map_expand()

--- src/hash_map.c
+++ src/hash_map.c
@@ -5,25 +5,19 @@
 
 #define HASH_MAP_MIN_CAP 16
 
-static bool hash_map_grow(HashMap* map, size_t cap)
+static bool hash_map_expand(HashMap* map)
 {
-    assert(cap && ((cap & (cap - 1)) == 0));
-
-    if (map->cap >= cap) {
-        return true;
+    size_t cap = map->cap * 2;
+    if (cap < HASH_MAP_MIN_CAP) {
+        cap = HASH_MAP_MIN_CAP;
     }
 
-    size_t new_cap = cap;
-    if (new_cap < HASH_MAP_MIN_CAP) {
-        new_cap = HASH_MAP_MIN_CAP;
-    }
-
-    HashMapKey* keys = new_array(map->allocator, HashMapKey, new_cap, true);
+    HashMapKey* keys = new_array(map->allocator, HashMapKey, cap, true);
     if (!keys) {
         return false;
     }
 
-    HashMapValues* values = new_array(map->allocator, HashMapValue, new_cap, false);
+    HashMapValues* values = new_array(map->allocator, HashMapValue, cap, false);
     if (!values) {
         mem_free(map->allocator, keys);
         return false;
@@ -35,7 +29,7 @@ static bool hash_map_grow(HashMap* map, size_t cap)
 
     map->keys = keys;
     map->values = values;
-    map->cap = new_cap;
+    map->cap = cap;
     map->len = 0;
 
     for (size_t i = 0; i < old_cap; ++i) {
