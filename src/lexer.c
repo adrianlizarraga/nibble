ad05cbcd336eeff49374fd0a5b40b507a8dbe520
adrianlizarraga <adrianlm2@gmail.com>
2021-03-11 01:22:18 -0800

    Renamed char prop printer tool; Made char table lookup smaller

--- src/lexer.c
+++ src/lexer.c
@@ -12,7 +12,7 @@
 
 // Converts a numeric character to an integer value. Values are biased by +1
 // so that a result of 0 is known to be invalid.
-static const unsigned int char_to_biased_digit[256] = {
+static const unsigned char char_to_biased_digit[256] = {
     ['0'] = 1,  ['1'] = 2,  ['2'] = 3,  ['3'] = 4,  ['4'] = 5,  ['5'] = 6,  ['6'] = 7,  ['7'] = 8,
     ['8'] = 9,  ['9'] = 10, ['a'] = 11, ['b'] = 12, ['c'] = 13, ['d'] = 14, ['e'] = 15, ['f'] = 16,
     ['A'] = 11, ['B'] = 12, ['C'] = 13, ['D'] = 14, ['E'] = 15, ['F'] = 16,
