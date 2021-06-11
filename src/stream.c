1f9d17c9de8b740c736365f94d4f838658eea235
adrianlizarraga <adrianlm2@gmail.com>
2021-06-10 19:28:08 -0700

    Add string bucket list to hold text lines during code generation

--- src/stream.c
+++ src/stream.c
@@ -46,3 +46,55 @@ void add_byte_stream_chunk(ByteStream* stream, const char* buf, size_t size)
         }
     }
 }
+
+static StrBucket* sbl_add_bucket(StrBucketList* buckets)
+{
+    size_t alloc_size = offsetof(StrBucket, buf) + (buckets->bucket_cap * sizeof(char*));
+    StrBucket* bucket = mem_allocate(buckets->arena, alloc_size, DEFAULT_ALIGN, false);
+
+    bucket->next = NULL;
+    bucket->len = 0;
+
+    if (buckets->first)
+        buckets->last->next = bucket;
+    else
+        buckets->first = bucket;
+
+    buckets->last = bucket;
+
+    return bucket;
+}
+
+char** sbl_add(StrBucketList* buckets, const char* str, size_t len)
+{
+    assert(buckets->last);
+
+    StrBucket* bucket = buckets->last;
+
+    if (bucket->len == buckets->bucket_cap)
+        bucket = sbl_add_bucket(buckets);
+
+    char** bucket_elem = bucket->buf + bucket->len;
+
+    if (str)
+        *bucket_elem = mem_dup(buckets->arena, str, len + 1, DEFAULT_ALIGN);
+
+    bucket->len += 1;
+    buckets->num_strs += 1;
+
+    return bucket_elem;
+}
+
+StrBucketList* new_sbl(Allocator* arena, size_t bucket_cap)
+{
+    StrBucketList* buckets = alloc_type(arena, StrBucketList, false);
+
+    buckets->arena = arena;
+    buckets->bucket_cap = bucket_cap;
+    buckets->first = NULL;
+    buckets->last = NULL;
+
+    sbl_add_bucket(buckets);
+
+    return buckets;
+}
--- src/stream.h
+++ src/stream.h
@@ -1,20 +1,42 @@
 #ifndef NIBBLE_STREAM_H
 #define NIBBLE_STREAM_H
 #include <stddef.h>
+typedef struct ByteStreamChunk ByteStreamChunk;
+typedef struct ByteStream ByteStream;
 
-typedef struct ByteStreamChunk {
-    struct ByteStreamChunk* next;
+struct ByteStreamChunk {
+    ByteStreamChunk* next;
     size_t size;
     char buf[];
-} ByteStreamChunk;
+};
 
-typedef struct ByteStream {
+struct ByteStream {
     ByteStreamChunk* first;
     ByteStreamChunk* last;
     size_t count;
     Allocator* allocator;
-} ByteStream;
+};
 
 ByteStream byte_stream_create(Allocator* allocator);
 void add_byte_stream_chunk(ByteStream* stream, const char* buf, size_t size);
+
+typedef struct StrBucket StrBucket;
+typedef struct StrBucketList StrBucketList;
+
+struct StrBucket {
+    StrBucket* next;
+    size_t len;
+    char* buf[];
+};
+
+struct StrBucketList {
+    StrBucket* first;
+    StrBucket* last;
+    size_t bucket_cap;
+    size_t num_strs;
+    Allocator* arena;
+};
+
+StrBucketList* new_sbl(Allocator* arena, size_t bucket_cap);
+char** sbl_add(StrBucketList* buckets, const char* str, size_t len);
 #endif
