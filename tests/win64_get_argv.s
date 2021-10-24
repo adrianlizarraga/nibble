ebe384d85b6f46689266a39e0759272aa779e386
adrianlizarraga <adrianlm2@gmail.com>
2021-10-23 17:30:47 -0700

    Use the windows calling convention for test program

--- tests/win64_get_argv.s
+++ tests/win64_get_argv.s
@@ -4,277 +4,317 @@ extern LocalAlloc
 ; proc _nibble_get_win64_cmd_args(argc: ^int) => ^^char;
 ; Sets argc and returns argv
 ; argv should be freed with LocalFree()
+global _nibble_get_win64_cmd_args
 _nibble_get_win64_cmd_args:
-        push    r12
-        xor     eax, eax
-        push    rbp
-        mov     rbp, rdi
-        push    rbx
-        call    GetCommandLineA
-        cmp     BYTE PTR [rax], 34
-        mov     r12, rax
-        lea     rcx, [rax+1]
-        jne     .l_winargs_84
-.l_winargs_3:
-        mov     al, BYTE PTR [rcx]
-        test    al, al
-        je      .l_winargs_4
-        inc     rcx
-        cmp     al, 34
-        jne     .l_winargs_3
-        jmp     .l_winargs_4
-.l_winargs_84:
-        mov     rcx, rax
-.l_winargs_2:
-        mov     al, BYTE PTR [rcx]
-        test    al, -33
-        je      .l_winargs_4
-        cmp     al, 9
-        je      .l_winargs_4
-        inc     rcx
-        jmp     .l_winargs_2
-.l_winargs_9:
-        inc     rcx
-.l_winargs_4:
-        mov     al, BYTE PTR [rcx]
-        cmp     al, 32
-        je      .l_winargs_9
-        cmp     al, 9
-        je      .l_winargs_9
-        cmp     al, 1
-        mov     r8d, 3
-        sbb     esi, esi
-        xor     eax, eax
-        xor     edx, edx
-        add     esi, 2
-.l_winargs_11:
-        mov     dil, BYTE PTR [rcx]
-        test    dil, dil
-        je      .l_winargs_85
-        cmp     dil, 32
-        sete    r9b
-        cmp     dil, 9
-        sete    r10b
-        or      r9b, r10b
-        je      .l_winargs_14
-        test    dl, 1
-        jne     .l_winargs_14
-.l_winargs_12:
-        mov     al, BYTE PTR [rcx]
-        cmp     al, 32
-        jne     .l_winargs_86
-.l_winargs_15:
-        inc     rcx
-        jmp     .l_winargs_12
-.l_winargs_86:
-        cmp     al, 9
-        je      .l_winargs_15
-        test    al, al
-        je      .l_winargs_49
-        inc     esi
-        jmp     .l_winargs_49
-.l_winargs_14:
-        inc     rcx
-        cmp     dil, 92
-        jne     .l_winargs_17
-        inc     eax
-        jmp     .l_winargs_11
-.l_winargs_17:
-        cmp     dil, 34
-        jne     .l_winargs_50
-        and     eax, 1
-        cmp     eax, 1
-        adc     edx, 0
-.l_winargs_19:
-        cmp     BYTE PTR [rcx], 34
-        jne     .l_winargs_87
-        inc     edx
-        inc     rcx
-        jmp     .l_winargs_19
-.l_winargs_87:
-        mov     eax, edx
-        cdq
-        idiv    r8d
-        xor     eax, eax
-        cmp     edx, 2
-        jne     .l_winargs_11
-        jmp     .l_winargs_81
-.l_winargs_49:
-        xor     eax, eax
-.l_winargs_81:
-        xor     edx, edx
-        jmp     .l_winargs_11
-.l_winargs_50:
-        xor     eax, eax
-        jmp     .l_winargs_11
-.l_winargs_85:
-        xor     edx, edx
-.l_winargs_22:
-        cmp     BYTE PTR [r12+rdx], 0
-        je      .l_winargs_88
-        inc     rdx
-        jmp     .l_winargs_22
-.l_winargs_88:
-        lea     eax, [rsi+1]
-        xor     edi, edi
-        cdqe
-        lea     rbx, [0+rax*8]
-        lea     rsi, [rbx+1+rdx]
-        call    LocalAlloc
-        mov     r8, rax
-        test    rax, rax
-        je      .l_winargs_1
-        lea     rax, [rax+rbx]
-        xor     edx, edx
-.l_winargs_25:
-        mov     cl, BYTE PTR [r12+rdx]
-        lea     rsi, [rax+rdx]
-        test    cl, cl
-        je      .l_winargs_89
-        mov     BYTE PTR [rax+rdx], cl
-        inc     rdx
-        jmp     .l_winargs_25
-.l_winargs_89:
-        mov     BYTE PTR [rsi], 0
-        mov     QWORD PTR [r8], rax
-        cmp     BYTE PTR [rax], 34
-        jne     .l_winargs_27
-        inc     rax
-.l_winargs_28:
-        mov     cl, BYTE PTR [rax]
-        lea     rdx, [rax-1]
-        test    cl, cl
-        je      .l_winargs_29
-        inc     rax
-        cmp     cl, 34
-        je      .l_winargs_29
-        mov     BYTE PTR [rax-2], cl
-        jmp     .l_winargs_28
-.l_winargs_90:
-        cmp     cl, 9
-        je      .l_winargs_29
-.l_winargs_27:
-        mov     cl, BYTE PTR [rax]
-        mov     rdx, rax
-        inc     rax
-        test    cl, -33
-        jne     .l_winargs_90
-        test    cl, cl
-        cmove   rax, rdx
-.l_winargs_29:
-        mov     BYTE PTR [rdx], 0
-.l_winargs_32:
-        mov     cl, BYTE PTR [rax]
-        cmp     cl, 32
-        jne     .l_winargs_91
-.l_winargs_33:
-        inc     rax
-        jmp     .l_winargs_32
-.l_winargs_91:
-        cmp     cl, 9
-        je      .l_winargs_33
-        test    cl, cl
-        jne     .l_winargs_34
-        mov     QWORD PTR [r8+8], 0
-        mov     DWORD PTR [rbp+0], 1
-        jmp     .l_winargs_1
-.l_winargs_34:
-        inc     rdx
-        xor     esi, esi
-        xor     r9d, r9d
-        mov     r11d, 2
-        mov     QWORD PTR [r8+8], rdx
-.l_winargs_35:
-        mov     dil, BYTE PTR [rax]
-        test    dil, dil
-        je      .l_winargs_92
-        cmp     dil, 32
-        lea     r10, [rax+1]
-        sete    cl
-        cmp     dil, 9
-        sete    bl
-        or      cl, bl
-        je      .l_winargs_36
-        test    r9b, 1
-        jne     .l_winargs_36
-        mov     BYTE PTR [rdx], 0
-        lea     rcx, [rdx+1]
-        mov     r10, rax
-.l_winargs_72:
-        mov     al, BYTE PTR [r10+1]
-        inc     r10
-        cmp     al, 32
-        je      .l_winargs_72
-        cmp     al, 9
-        je      .l_winargs_72
-        test    al, al
-        je      .l_winargs_53
-        movsx   rax, r11d
-        inc     r11d
-        mov     QWORD PTR [r8+rax*8], rcx
-        jmp     .l_winargs_53
-.l_winargs_36:
-        cmp     dil, 92
-        jne     .l_winargs_39
-        mov     BYTE PTR [rdx], 92
-        lea     rcx, [rdx+1]
-        inc     esi
-        jmp     .l_winargs_38
-.l_winargs_39:
-        cmp     dil, 34
-        jne     .l_winargs_40
-        mov     eax, esi
-        sar     eax
-        and     sil, 1
-        cdqe
-        jne     .l_winargs_41
-        sub     rdx, rax
-        inc     r9d
-        mov     rcx, rdx
-        jmp     .l_winargs_43
-.l_winargs_41:
-        not     rax
-        add     rax, rdx
-        mov     BYTE PTR [rax], 34
-        lea     rcx, [rax+1]
-        jmp     .l_winargs_43
-.l_winargs_45:
-        inc     r9d
-        cmp     r9d, 3
-        jne     .l_winargs_44
-        mov     BYTE PTR [rcx], 34
-        xor     r9d, r9d
-        inc     rcx
-.l_winargs_44:
-        inc     r10
-.l_winargs_43:
-        cmp     BYTE PTR [r10], 34
-        je      .l_winargs_45
-        xor     esi, esi
-        cmp     r9d, 2
-        jne     .l_winargs_38
-        jmp     .l_winargs_82
-.l_winargs_40:
-        mov     BYTE PTR [rdx], dil
-        lea     rcx, [rdx+1]
-        xor     esi, esi
-        jmp     .l_winargs_38
-.l_winargs_53:
-        xor     esi, esi
-.l_winargs_82:
-        xor     r9d, r9d
-.l_winargs_38:
-        mov     rdx, rcx
-        mov     rax, r10
-        jmp     .l_winargs_35
-.l_winargs_92:
-        movsx   rax, r11d
-        mov     BYTE PTR [rdx], 0
-        mov     QWORD PTR [r8+rax*8], 0
-        mov     DWORD PTR [rbp+0], r11d
-.l_winargs_1:
-        pop     rbx
-        mov     rax, r8
-        pop     rbp
-        pop     r12
-        ret
+    push    r14
+    push    rsi
+    push    rdi
+    push    rbx
+    sub     rsp, 40
+    mov     r14, rcx
+    call    GetCommandLineA
+    mov     rdi, rax
+    mov     r8b, byte [rax]
+    cmp     r8b, 34
+    jne     .L_win64_args_1
+    lea     rcx, [rdi + 1]
+.L_win64_args_6:
+    mov     al, byte [rcx]
+    test    al, al
+    je      .L_win64_args_8
+    inc     rcx
+    cmp     al, 34
+    jne     .L_win64_args_6
+    jmp     .L_win64_args_8
+.L_win64_args_1:
+    movabs  rax, 4294967809
+    mov     edx, r8d
+    mov     rcx, rdi
+.L_win64_args_2:
+    cmp     dl, 32
+    ja      .L_win64_args_4
+    movzx   edx, dl
+    bt      rax, rdx
+    jb      .L_win64_args_8
+.L_win64_args_4:
+    mov     dl, byte [rcx + 1]
+    inc     rcx
+    jmp     .L_win64_args_2
+.L_win64_args_86:
+    inc     rcx
+.L_win64_args_8:
+    mov     al, byte [rcx]
+    cmp     al, 9
+    je      .L_win64_args_86
+    cmp     al, 32
+    je      .L_win64_args_86
+    test    al, al
+    jne     .L_win64_args_12
+    mov     edx, 1
+    jmp     .L_win64_args_13
+.L_win64_args_12:
+    mov     edx, 2
+.L_win64_args_13:
+    xor     esi, esi
+    xor     ebx, ebx
+.L_win64_args_14:
+    cmp     al, 32
+    je      .L_win64_args_23
+    test    al, al
+    je      .L_win64_args_16
+    cmp     al, 9
+    jne     .L_win64_args_20
+    test    esi, esi
+    je      .L_win64_args_24
+.L_win64_args_20:
+    cmp     al, 34
+    je      .L_win64_args_28
+    cmp     al, 92
+    jne     .L_win64_args_31
+    inc     ebx
+    inc     rcx
+    jmp     .L_win64_args_33
+.L_win64_args_23:
+    test    esi, esi
+    je      .L_win64_args_24
+.L_win64_args_31:
+    inc     rcx
+    jmp     .L_win64_args_32
+.L_win64_args_87:
+    mov     al, byte [rcx + 1]
+    inc     rcx
+.L_win64_args_24:
+    cmp     al, 9
+    je      .L_win64_args_87
+    cmp     al, 32
+    je      .L_win64_args_87
+    test    al, al
+    je      .L_win64_args_32
+    inc     edx
+.L_win64_args_32:
+    xor     ebx, ebx
+.L_win64_args_33:
+    mov     al, byte [rcx]
+    jmp     .L_win64_args_14
+.L_win64_args_28:
+    not     ebx
+    and     ebx, 1
+    lea     eax, [rsi + rbx]
+    dec     eax
+.L_win64_args_29:
+    inc     eax
+    cmp     byte [rcx + 1], 34
+    lea     rcx, [rcx + 1]
+    je      .L_win64_args_29
+    movsxd  rsi, eax
+    imul    rax, rsi, 1431655766
+    mov     rbx, rax
+    shr     rbx, 63
+    shr     rax, 32
+    add     eax, ebx
+    lea     eax, [rax + 2*rax]
+    sub     esi, eax
+    xor     ebx, ebx
+    cmp     esi, 2
+    cmove   esi, ebx
+    jmp     .L_win64_args_33
+.L_win64_args_16:
+    test    r8b, r8b
+    je      .L_win64_args_17
+    mov     ecx, 1
+.L_win64_args_35:
+    lea     rax, [rcx + 1]
+    cmp     byte [rdi + rcx], 0
+    mov     rcx, rax
+    jne     .L_win64_args_35
+    jmp     .L_win64_args_36
+.L_win64_args_17:
+    mov     eax, 1
+.L_win64_args_36:
+    movsxd  rsi, edx
+    lea     rdx, [rax + 8*rsi]
+    add     rdx, 8
+    xor     ecx, ecx
+    call    LocalAlloc
+    test    rax, rax
+    je      .L_win64_args_37
+    lea     rsi, [rax + 8*rsi]
+    add     rsi, 8
+    mov     cl, byte [rdi]
+    test    cl, cl
+    je      .L_win64_args_39
+    inc     rdi
+    mov     rdx, rsi
+.L_win64_args_41:
+    mov     byte [rdx], cl
+    inc     rdx
+    mov     cl, byte [rdi]
+    inc     rdi
+    test    cl, cl
+    jne     .L_win64_args_41
+    jmp     .L_win64_args_42
+.L_win64_args_37:
+    xor     eax, eax
+    jmp     .L_win64_args_85
+.L_win64_args_39:
+    mov     rdx, rsi
+.L_win64_args_42:
+    mov     byte [rdx], 0
+    mov     qword [rax], rsi
+    mov     cl, byte [rsi]
+    cmp     cl, 34
+    jne     .L_win64_args_43
+.L_win64_args_48:
+    mov     cl, byte [rsi + 1]
+    test    cl, cl
+    je      .L_win64_args_52
+    cmp     cl, 34
+    je      .L_win64_args_50
+    mov     byte [rsi], cl
+    inc     rsi
+    jmp     .L_win64_args_48
+.L_win64_args_43:
+    movabs  rdx, 4294967809
+.L_win64_args_44:
+    cmp     cl, 32
+    ja      .L_win64_args_46
+    movzx   ebx, cl
+    bt      rdx, rbx
+    jb      .L_win64_args_51
+.L_win64_args_46:
+    mov     cl, byte [rsi + 1]
+    inc     rsi
+    jmp     .L_win64_args_44
+.L_win64_args_51:
+    cmp     cl, 1
+    mov     rcx, rsi
+    sbb     rcx, -1
+    jmp     .L_win64_args_53
+.L_win64_args_52:
+    lea     rcx, [rsi + 1]
+    jmp     .L_win64_args_53
+.L_win64_args_50:
+    lea     rcx, [rsi + 2]
+.L_win64_args_53:
+    mov     byte [rsi], 0
+    inc     rsi
+.L_win64_args_54:
+    mov     dl, byte [rcx]
+    cmp     dl, 9
+    je      .L_win64_args_88
+    cmp     dl, 32
+    jne     .L_win64_args_56
+.L_win64_args_88:
+    inc     rcx
+    jmp     .L_win64_args_54
+.L_win64_args_56:
+    test    dl, dl
+    jne     .L_win64_args_58
+    mov     rcx, rax
+    add     rcx, 8
+    mov     r9d, 1
+    jmp     .L_win64_args_84
+.L_win64_args_58:
+    mov     qword [rax + 8], rsi
+    xor     r8d, r8d
+    mov     r9d, 2
+    xor     edi, edi
+.L_win64_args_82:
+    xor     ebx, ebx
+.L_win64_args_60:
+    mov     dl, byte [rcx]
+    cmp     dl, 32
+    je      .L_win64_args_68
+    test    dl, dl
+    je      .L_win64_args_83
+    cmp     dl, 9
+    jne     .L_win64_args_64
+    test    edi, edi
+    je      .L_win64_args_69
+.L_win64_args_64:
+    cmp     dl, 92
+    jne     .L_win64_args_65
+    inc     rcx
+    mov     byte [rsi], 92
+    inc     rsi
+    inc     ebx
+    jmp     .L_win64_args_60
+.L_win64_args_68:
+    test    edi, edi
+    jne     .L_win64_args_81
+.L_win64_args_69:
+    mov     byte [rsi], 0
+    inc     rsi
+.L_win64_args_70:
+    inc     rcx
+    mov     dl, byte [rcx]
+    cmp     dl, 9
+    je      .L_win64_args_70
+    cmp     dl, 32
+    je      .L_win64_args_70
+    test    dl, dl
+    je      .L_win64_args_82
+    mov     edx, r9d
+    inc     r9d
+    mov     qword [rax + 8*rdx], rsi
+    jmp     .L_win64_args_82
+.L_win64_args_65:
+    cmp     dl, 34
+    je      .L_win64_args_66
+.L_win64_args_81:
+    inc     rcx
+    mov     byte [rsi], dl
+    inc     rsi
+    jmp     .L_win64_args_82
+.L_win64_args_66:
+    mov     edx, ebx
+    shr     edx, 31
+    add     edx, ebx
+    sar     edx
+    neg     edx
+    movsxd  rdx, edx
+    test    bl, 1
+    jne     .L_win64_args_74
+    add     rsi, rdx
+    inc     edi
+    jmp     .L_win64_args_75
+.L_win64_args_74:
+    mov     byte [rdx + rsi - 1], 34
+    add     rsi, rdx
+.L_win64_args_75:
+    lea     rbx, [rcx + 1]
+    cmp     byte [rcx + 1], 34
+    jne     .L_win64_args_76
+.L_win64_args_77:
+    inc     edi
+    cmp     edi, 3
+    jne     .L_win64_args_79
+    mov     byte [rsi], 34
+    inc     rsi
+    xor     edi, edi
+.L_win64_args_79:
+    lea     rcx, [rbx + 1]
+    cmp     byte [rbx + 1], 34
+    mov     rbx, rcx
+    je      .L_win64_args_77
+    jmp     .L_win64_args_80
+.L_win64_args_76:
+    mov     rcx, rbx
+.L_win64_args_80:
+    cmp     edi, 2
+    cmove   edi, r8d
+    jmp     .L_win64_args_82
+.L_win64_args_83:
+    mov     byte [rsi], 0
+    mov     ecx, r9d
+    lea     rcx, [rax + 8*rcx]
+.L_win64_args_84:
+    mov     qword [rcx], 0
+    mov     dword [r14], r9d
+.L_win64_args_85:
+    add     rsp, 40
+    pop     rbx
+    pop     rdi
+    pop     rsi
+    pop     r14
+    ret
