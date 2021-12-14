17cb6d867764fb9f778048293599c293eb743426
adrianlizarraga <adrianlm2@gmail.com>
2021-12-14 13:07:14 -0800

    Add more ir structs

--- src/ir.h
+++ src/ir.h
@@ -87,5 +87,66 @@ typedef struct InstrRet {
     int a;
 } InstrRet;
 
+typedef struct InstrConvert {
+    Type* dst_type;
+    Type* src_type;
+    int r;
+    int a;
+} InstrConvert;
+
+typedef enum ConditionKind {
+    COND_U_LT,
+    COND_S_LT,
+    COND_U_LTEQ,
+    COND_S_LTEQ,
+    COND_U_GT,
+    COND_S_GT,
+    COND_U_GTEQ,
+    COND_S_GTEQ,
+    COND_EQ,
+    COND_NEQ,
+} ConditionKind;
+
+typedef struct InstrCmp {
+    Type* type;
+    ConditionKind cond;
+    int r;
+    int a;
+    int b;
+} InstrCmp;
+
+typedef struct InstrJmp {
+    u32* jmp_target;
+} InstrJmp;
+
+typedef struct InstrCallArg {
+    Type* type;
+    int loc; // TODO: Support stack args
+} InstrCallArg;
+
+typedef struct InstrCall {
+    Symbol* sym;
+    int r;
+    u32 num_args;
+    InstrCallArg* args;
+} InstrCall;
+
+typedef struct InstrCallIndirect {
+    Type* proc_type;
+    int loc;
+    int r;
+    u32 num_args;
+    InstrCallArg* args;
+} InstrCallIndirect;
+
+typedef struct InstrMemcpy {
+    Type* type;
+    MemAddr dst;
+    MemAddr src;
+} InstrMemcpy;
+
+typedef struct Instr {
+    InstrKind kind;
+} Instr;
 
 #endif
