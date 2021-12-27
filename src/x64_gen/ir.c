4ff8be08d089f284ea8992f65132809c777ccb0d
adrianlizarraga <adrianlm2@gmail.com>
2021-12-27 03:29:57 -0800

    Try to convert IR div instr to LLIR x64 idiv and div

--- src/x64_gen/ir.c
+++ src/x64_gen/ir.c
@@ -39,6 +39,7 @@ typedef struct X64_LLIRBuilder {
     X64_Instr** instrs; // Stretchy buf
     X64_RegRange* llir_ranges; // Stretchy buf
     u32* reg_map; // Map IR reg -> LLIR reg; size: num_iregs
+    u32* phys_reg_map; // Map LLIR reg -> Physical reg; size: num_llirregs
 
     // Disjoint Set Union data structure for register renaming/aliasing.
     u32* llir_aliases; // Root alias node for each llir reg. size: num_llirregs
@@ -52,6 +53,7 @@ static void X64_init_llir_builder(X64_LLIRBuilder* builder, Allocator* arena, u3
     builder->llir_ranges = array_create(arena, X64_RegRange, 16);
     builder->llir_aliases = array_create(arena, u32, 16);
     builder->llir_sizes = array_create(arena, u32, 16);
+    builder->phys_reg_map = array_create(arena, X64_Reg, 16);
 
     builder->reg_map = alloc_array(arena, u32, num_irregs, false);
     memset(builder->reg_map, 0xFF, num_iiregs * sizeof(u32));
@@ -110,6 +112,7 @@ static u32 X64_next_llir_reg(X64_LLIRBuilder* builder)
     X64_Range range = {.start = curr_ip, .end = curr_ip};
 
     array_push(builder->llir_ranges, range);
+    array_push(builder->phys_reg_map, X64_REG_COUNT);
 
     u32 next_reg = array_len(builder->llir_ranges) - 1;
     assert(next_reg < (u32)-1);
@@ -137,15 +140,27 @@ static u32 X64_get_llir_reg(X64_LLIRBuilder* builder, u32 irreg)
     return result;
 }
 
+static u32 X64_get_phys_reg(X64_LLIRBuilder* builder, X64_Reg phys_reg)
+{
+    u32 result = X64_next_llir_reg(builder);
+
+    builder->phys_reg_map[result] = phys_reg;
+}
+
 static void X64_emit_llir_instr(X64_LLIRBuilder* builder, Instr* ir_instr)
 {
     static X64_InstrKind binary_kind[] = {
-        [INSTR_ADD] = [X64_INSTR_ADD_R_R],
-        [INSTR_SUB] = [X64_INSTR_SUB_R_R],
-        [INSTR_MUL] = [X64_INSTR_IMUL_R_R],
-        [INSTR_AND] = [X64_INSTR_AND_R_R],
-        [INSTR_OR]  = [X64_INSTR_OR_R_R],
-        [INSTR_XOR] = [X64_INSTR_XOR_R_R]
+        [INSTR_ADD] = X64_INSTR_ADD_R_R,
+        [INSTR_SUB] = X64_INSTR_SUB_R_R,
+        [INSTR_MUL] = X64_INSTR_IMUL_R_R,
+        [INSTR_AND] = X64_INSTR_AND_R_R,
+        [INSTR_OR]  = X64_INSTR_OR_R_R,
+        [INSTR_XOR] = X64_INSTR_XOR_R_R
+    };
+
+    static X64_InstrKind div_kind[] = {
+        [INSTR_UDIV] = X64_INSTR_DIV,
+        [INSTR_SDIV] = X64_INSTR_IDIV
     };
 
     switch (ir_instr->kind) {
@@ -159,14 +174,43 @@ static void X64_emit_llir_instr(X64_LLIRBuilder* builder, Instr* ir_instr)
         //
         // mov r, a
         // add r, b
+        size_t size = ir_instr->binary.type->size;
 
         u32 r = X64_get_llir_reg(builder, ir_instr->binary.r);
         u32 a = X64_get_llir_reg(builder, ir_instr->binary.a);
         u32 b = X64_get_llir_reg(builder, ir_instr->binary.b);
 
-        X64_emit_mov_r_r(builder, ir_instr->binary.type, r, a);
-        X64_emit_binary_r_r(builder, binary_kind[ir_instr->kind], ir_instr->binary.type, r, b);
+        X64_emit_mov_r_r(builder, size, r, a);
+        X64_emit_binary_r_r(builder, binary_kind[ir_instr->kind], size, r, b);
 
+        break;
+    }
+    case INSTR_UDIV:
+    case INSTR_SDIV: {
+
+        // EX: r = a / b
+        //
+        // mov _ax, a
+        // cqo        ; sign extend into _dx if size >= 2 bytes
+        // div b
+        // mov r, _ax
+
+        size_t size = ir_instr->binary.type->size;
+
+        u32 ax = X64_get_phys_reg(builder, X64_RAX);
+        u32 a = X64_get_llir_reg(builder, ir_instr->binary.a);
+        X64_emit_mov_r_r(builder, size, ax, a);
+
+        if (size >= 2) { // Reserve rdx
+            X64_get_phys_reg(builder, X64_RDX);
+            X64_emit_sext_ax_to_dx(builder, size);
+        }
+
+        u32 b = X64_get_llir_reg(builder, ir_instr->binary.b);
+        X64_emit_div_r(builder, div_kind[ir_instr->kind], size, b);
+        
+        u32 r = X64_get_llir_reg(builder, ir_instr->binary.r);
+        X64_emit_mov_r_r(builder, size, r, ax);
         break;
     }
     }
