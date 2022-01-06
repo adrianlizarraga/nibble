8335cdef8d26d5c46137b09898952bad956aaf1e
adrianlizarraga <adrianlm2@gmail.com>
2022-01-05 23:14:26 -0800

    Use builder for Lir creation funcs

--- src/x64_gen/lir.c
+++ src/x64_gen/lir.c
@@ -1,6 +1,16 @@
 #include "x64_gen/lir.h"
 
-X64_new_instr(Allocator* arena, X64_InstrKind kind)
+void X64_add_lir_instr(X64_LIRBuilder* builder, X64_Instr* instr)
+{
+    if (builder->next_instr_is_jmp_target) {
+        instr->is_jmp_target = true;
+        builder->next_instr_is_jmp_target = false;
+    }
+
+    array_push(builder->instrs, instr);
+}
+
+X64_Instr* X64_new_instr(Allocator* arena, X64_InstrKind kind)
 {
     X64_Instr* instr = alloc_type(arena, X64_Instr, true);
     instr->kind = kind;
@@ -8,9 +18,9 @@ X64_new_instr(Allocator* arena, X64_InstrKind kind)
     return instr;
 }
 
-X64_Instr* X64_new_instr_binary_r_r(Allocator* arena, X64_InstrKind kind, size_t size, u32 dst, u32 src)
+void X64_emit_instr_binary_r_r(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 dst, u32 src)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->binary_r_r.size = size;
     instr->binary_r_r.dst = dst;
     instr->binary_r_r.src = src;
@@ -18,9 +28,9 @@ X64_Instr* X64_new_instr_binary_r_r(Allocator* arena, X64_InstrKind kind, size_t
     return instr;
 }
 
-X64_Instr* X64_new_instr_binary_r_i(Allocator* arena, X64_InstrKind kind, size_t size, u32 dst, Scalar src)
+void X64_emit_instr_binary_r_i(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 dst, Scalar src)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->binary_r_i.size = size;
     instr->binary_r_i.dst = dst;
     instr->binary_r_i.src = src;
@@ -28,9 +38,9 @@ X64_Instr* X64_new_instr_binary_r_i(Allocator* arena, X64_InstrKind kind, size_t
     return instr;
 }
 
-X64_Instr* X64_new_instr_shift_r_r(Allocator* arena, X64_InstrKind kind, size_t size, u32 dst, u32 src)
+void X64_emit_instr_shift_r_r(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 dst, u32 src)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->binary_r_r.size = size;
     instr->binary_r_r.dst = dst;
     instr->binary_r_r.src = src;
@@ -38,9 +48,9 @@ X64_Instr* X64_new_instr_shift_r_r(Allocator* arena, X64_InstrKind kind, size_t 
     return instr;
 }
 
-X64_Instr* X64_new_instr_shift_r_i(Allocator* arena, X64_InstrKind kind, size_t size, u32 dst, Scalar src)
+void X64_emit_instr_shift_r_i(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 dst, Scalar src)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->binary_r_i.size = size;
     instr->binary_r_i.dst = dst;
     instr->binary_r_i.src = src;
@@ -48,27 +58,27 @@ X64_Instr* X64_new_instr_shift_r_i(Allocator* arena, X64_InstrKind kind, size_t 
     return instr;
 }
 
-X64_Instr* X64_new_instr_div_r(Allocator* arena, X64_InstrKind kind, size_t size, u32 src)
+void X64_emit_instr_div_r(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 src)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->div_r.size = size;
     instr->div_r.src = src;
 
     return instr;
 }
 
-X64_Instr* X64_new_instr_unary(Allocator* arena, X64_InstrKind kind, size_t size, u32 dst)
+void X64_emit_instr_unary(X64_LIRBuilder* builder, X64_InstrKind kind, size_t size, u32 dst)
 {
-    X64_Instr* instr = X64_new_instr(arena, kind);
+    X64_Instr* instr = X64_new_instr(builder->arena, kind);
     instr->unary.size = size;
     instr->unary.dst = dst;
 
     return instr;
 }
 
-X64_Instr* X64_new_instr_mov_r_r(Allocator* arena, size_t size, u32 dst, u32 src)
+void X64_emit_instr_mov_r_r(X64_LIRBuilder* builder, size_t size, u32 dst, u32 src)
 {
-    X64_Instr* instr = X64_new_instr(arena, X64_INSTR_MOV_R_R);
+    X64_Instr* instr = X64_new_instr(builder->arena, X64_INSTR_MOV_R_R);
     instr->mov_r_r.size = size;
     instr->mov_r_r.dst = dst;
     instr->mov_r_r.src = src;
