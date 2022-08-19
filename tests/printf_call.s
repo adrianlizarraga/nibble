691ee7fc1fe3ab9e55584f1a75d57d6b8950b83c
adrianlizarraga <adrianlm2@gmail.com>
2022-08-19 02:31:47 -0700

    Modify asm example that uses libc so that can examine generated elf from nasm

--- tests/printf_call.s
+++ tests/printf_call.s
@@ -1,18 +1,28 @@
-extern printf
+; nasm -f elf64 tests/printf_call.s -o out_p.o
+; ld out_p.o -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -pie -o out_p
+default rel
+extern puts
 
 SECTION .rodata
 string: db "hello nibble\n", 0
 
+SECTION .data
+g_x: db 10
+g_y: db 11
+
 SECTION .text
-global main
-main:
-    push rbp
-    mov rbp, rsp
+global _start
+_start:
+  xor rbp, rbp
 
-    xor eax, eax  ; No vector args to variadic function
-    lea rdi, [rel string]
-    call printf
+  mov eax, [rel g_x]
+  mov eax, [rel g_y]
+  xor eax, eax  ; No vector args to variadic function
+  lea rdi, [rel string]
+  call puts wrt ..plt
+  ;call [rel puts wrt ..got]
+  ;call puts ; Linker error
 
-    xor eax, eax ; Return 0
-    pop rbp
-    ret
+  xor eax, eax ; Return 0
+  mov rax, 60
+  syscall ; exit
