e63c8a1ac87b113a97784f48593bc94f14d72a91
adrianlizarraga <adrianlm2@gmail.com>
2023-12-16 16:04:24 -0800

    Call nasm from within elf_gen test file

--- tests/elf_gen/main.c
+++ tests/elf_gen/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "basics.h"
 #include "allocator.h"
+#include "os_utils.h"
 #include "x64_gen/x64_instrs.h"
 #include "x64_gen/machine_code.h"
 
@@ -11,7 +12,7 @@ int main(int argc, char** argv) {
 
     X64_emit_instr_add_ri(&x64_instrs, 4, X64_RAX, 10);
     X64_emit_instr_jmp(&x64_instrs, 0);
-    printf("Num bblocks = %lu, Num instrs = %d\n", array_len(x64_instrs.bblocks), x64_instrs.num_instrs);
+    ftprint_out("Num bblocks = %lu, Num instrs = %d\n", array_len(x64_instrs.bblocks), x64_instrs.num_instrs);
 
     Array(u8) buffer = array_create(&alloc, u8, 64);
     Array(X64_TextReloc) relocs = array_create(&alloc, X64_TextReloc, 2);
@@ -20,11 +21,32 @@ int main(int argc, char** argv) {
     X64_elf_gen_instrs(&alloc, &x64_instrs, &buffer, &relocs, &proc_off_patches);
 
     // Print bytes.
-    printf("Instruction bytes:\n");
+    ftprint_out("Instruction bytes:\n");
     for (size_t i = 0; i < array_len(buffer); i++) {
-        printf("0x%X ", buffer[i]);
+        ftprint_out("0x%X ", buffer[i]);
     }
-    printf("\n");
+    ftprint_out("\n");
+
+    FILE* test_asm_fd = fopen("test.s", "w");
+    if (!test_asm_fd) {
+        ftprint_out("[ERROR]: failed to open test.s for writing\n");
+        allocator_destroy(&alloc);
+        return 1;
+    }
+
+    ftprint_file(test_asm_fd, false, "l0: add eax, 0xA\njmp l0");
+    fclose(test_asm_fd);
+
+    const char* nasm_cmd_argv[] = {"nasm", "-f", "elf64", "test.s", "-o", "test.o", NULL};
+    ExecCmd nasm_cmd = {.argv = nasm_cmd_argv, .argc = ARRAY_LEN(nasm_cmd_argv) - 1};
+    if (run_cmd(&alloc, &nasm_cmd, false) != 0) {
+        ftprint_out("[ERROR]: Failed to run NASM command\n");
+        allocator_destroy(&alloc);
+        return 1;
+    }
+
+    // TODO: Get bytes from test.o and compare with our generated buffer
+
     allocator_destroy(&alloc);
     return 0;
 }
